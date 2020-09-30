4590d7027b4ed656549223e0b55e87dbcd512c70
Ivan D Cuadro <ivan.cuadro@wolox.co>
2020-09-30 00:46:08 -0500

    fix: Disabled eslint ANY type validation

--- .eslintrc.js
+++ .eslintrc.js
@@ -15,6 +15,7 @@ module.exports = {
   },
   rules: {
     'no-console': 'error',
+    '@typescript-eslint/no-explicit-any': 'off',
     '@typescript-eslint/no-var-requires': 0
   },
   settings: {
--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,7 @@
 import React from 'react';
 
-const App = () => {
-  return (
-    <div className="document">
-      Welcome to Application
-    </div>
-  )
-}
+const App = (): any => {
+  return <div className="document">Welcome to Application 2</div>;
+};
 
 export default App;
