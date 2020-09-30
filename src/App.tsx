adde7567f06a3264f17239f25cfccc8b0625ef81
Ivan D Cuadro <ivan.cuadro@wolox.co>
2020-09-30 02:03:20 -0500

    feature: CSS reset

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
 import React from 'react';
 
 const App = (): any => {
-  return <div className="document">Welcome to Application 2</div>;
+  return (
+    <div className="document">
+      <h1>Welcome to Application</h1>
+    </div>
+  );
 };
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App.tsx';
+import './index.css';
 
 const rootNode = document.getElementById('root');
 rootNode ? ReactDOM.render(<App />, document.getElementById('root')) : false;
